22de814bed49c95c17f5e6c09821349f2d94af4f
ProgramFOX <programfox@hotmail.be>
2015-12-04 13:40:17 +0100

    Add doubleflip command

--- fun.py
+++ fun.py
@@ -12,9 +12,8 @@ eyes = ['°', '゜', 'ಥ', "'", '•', '^', '⇀', 'ಠ', '๑']
 mouths = ['□', 'Д', '益', 'ᴥ', '.', 'ʖ', 'ل͜', '³', 'ਊ']
 arms = ['╯', 'ง', '┛', 'づ']
 
+
 def exec_flip(cmd, bot, args, msg, event):
-    global eyes
-    global mouths
     if len(args) == 0:
         return 'Not enough arguments'
     left_eye = random.choice(eyes)
@@ -29,6 +28,27 @@ def exec_flip(cmd, bot, args, msg, event):
     return '(' + left_arm + face + ')' + right_arm + '︵' + flipped
 
 
+def exec_doubleflip(cmd, bot, args, msg, event):
+    if len(args) == 0:
+        return 'Not enough arguments'
+    inp = ' '.join(args)
+    mirrored = ''.join(reversed(inp))
+    mirrored_flipped = upsidedown.transform(mirrored)
+    input_flipped = upsidedown.transform(inp)
+    
+    left_eye = random.choice(eyes)
+    right_eye = random.choice(eyes + [left_eye] * 20)
+    mouth = random.choice(mouths)
+    left_arm = random.choice(arms)
+    right_arm = random.choice(arms + [left_arm] * 3)
+    face = left_eye + mouth + right_eye
+    body = left_arm + '(' + face + ')' + right_arm
+
+    return mirrored_flipped + ' ' + body + ' ' + input_flipped
+
+
+
 commands = [
-    Command('flip', exec_flip, 'This command will flip anything you throw at it. Syntax: `$PREFIXflip something`', False, False, None, None, None, None)
+    Command('flip', exec_flip, 'This command will flip anything you throw at it. Syntax: `$PREFIXflip something`', False, False, None, None, None, None),
+    Command('doubleflip', exec_doubleflip, 'This command will double-flip anything you throw at it. Syntax: `$PREFIXdoubleflip something`', False, False, None, None, None, None)
 ]
