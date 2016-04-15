b492339a063c28487dd77eeecda325882e3fbf81
ProgramFOX <programfox@hotmail.be>
2016-04-15 17:50:38 +0200

    Fix TypeError by removing redundant argument

--- didyoumean.py
+++ didyoumean.py
@@ -52,7 +52,7 @@ def on_bot_load(bot):
                 suggestions[event.user.id] = None
                 return "Command not found."
             else:
-                suggestions[event.user.id] = dym + " " + " ".join(cmd.split(cmd, " ")[1:])
+                suggestions[event.user.id] = dym + " " + " ".join(cmd.split(" ")[1:])
                 return "Command not found. Did you mean: `%s`?" % dym
         else:
             suggestions[event.user.id] = None
